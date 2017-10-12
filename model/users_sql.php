9d61533666dff42557e839b975114b35641ac4da
User-02 <user-02@Trainings-Mac-mini-70.local>
2017-10-12 15:37:15 +0300

    Log out check

--- model/users_sql.php
+++ model/users_sql.php
@@ -1,5 +1,8 @@
 <?php
 
+//Include connection
+require_once "sqlConnection.php";
+
 //Function to check if user exists
 
 /**
@@ -35,5 +38,50 @@ function checkUserExist($email)
 
         //HAVE TO ADD ERROR PAGE
         return false;
+    }
+}
+
+
+//Function for checking if user entered correct password
+
+
+function checkPass($logingUserId, $password){
+
+    try{
+
+        //Get PDO connection, using the function above
+        $pdo = getConnection();
+
+        //If Password is null, check only username(for registration) otherwise check both(for login)
+        //Check for Registration
+        if ($password == null) {
+            //Prepare statement to execute in the Database(prevention of Database injection)
+            $statement = $pdo->prepare("SELECT 'name' FROM users WHERE 'name' = ?");
+            //Execute statement
+            $statement->execute(array($username));
+            //Check if Database returned a user (1 or 0 Columns)
+            if ($statement->rowCount()) {
+                return true;
+            } else {
+                return false;
+            }
+            //Check for Login
+        } else {
+            //Prepare statement to execute in the Database(prevention of Database injection)
+            $statement = $pdo->prepare("SELECT user_id, name, password FROM users WHERE name = ? AND password = ?");
+            //Execute statement
+            $statement->execute(array($username, $password));
+            //Check if Database returned a user (1 or 0 Columns)
+            if ($statement->rowCount()) {
+                //Fetch User ID and return it as result
+                $userInfo = $statement->fetchAll(PDO::FETCH_ASSOC);
+                return $userInfo[0]['user_id'];
+            } else {
+                return false;
+            }
+        }
+    } catch (Exception $e) {
+        //In case of PDO error, redirect to Error page
+        header("Location: ../view/error.php");
     }
 }
