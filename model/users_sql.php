ba4ac39e69c300cb6f521443f542a8d6a639721b
Your Name <lachezar.gadzhev@gmail.com>
2017-10-12 18:05:48 +0300

    Added registration function

--- model/users_sql.php
+++ model/users_sql.php
@@ -5,7 +5,6 @@
 require_once "sqlConnection.php";
 
 
-
 //Function to check if user exists
 
 /**
@@ -13,7 +12,8 @@ require_once "sqlConnection.php";
  * @return bool - returns the existing user ID or false if the user doesn't exist
  */
 
-function checkUserExist($email){
+function checkUserExist($email)
+{
 
     //Try the code for exceptions
     try {
@@ -55,30 +55,73 @@ function checkUserExist($email){
  * @return mixed - return user's id for session if password match or false if doesn't
  */
 
-function checkPass($logingUserId, $password){
+function checkPass($logingUserId, $password)
+{
 
     //Try the code for exceptions
-    try{
+    try {
 
         //Get PDO connection, using the function above
         $pdo = getConnection();
 
 
-            //Prepare statement to execute in the Database(prevention of Database injection)
-            $statement = $pdo->prepare("SELECT id, password FROM users WHERE id = ? AND password = ?");
+        //Prepare statement to execute in the Database(prevention of Database injection)
+        $statement = $pdo->prepare("SELECT id, password FROM users WHERE id = ? AND password = ?");
 
-            //Execute statement
-            $statement->execute(array($logingUserId, $password));
+        //Execute statement
+        $statement->execute(array($logingUserId, $password));
 
-            //Check if Database returned a user (1 or 0 Columns)
-            if ($statement->rowCount()) {
+        //Check if Database returned a user (1 or 0 Columns)
+        if ($statement->rowCount()) {
 
-                //Fetch User ID and return it as result
-                $userInfo = $statement->fetch();
-                return $userInfo['id'];
-            } else {
-                return false;
-            }
+            //Fetch User ID and return it as result
+            $userInfo = $statement->fetch();
+            return $userInfo['id'];
+        } else {
+            return false;
+        }
+
+        //Catch exception and perform action
+    } catch (PDOException $e) {
+
+        //HAVE TO ADD ERROR PAGE
+        return false;
+    }
+}
+
+
+//Function for registering user
+
+/**
+ * @param $email - email of the user
+ * @param null $firstName - first name of the user
+ * @param null $lastName - last name of the user
+ * @param null $mobilePhone - mobile phone of the user
+ * @param string $imageUrl - profile picture of the user
+ * @param $password - password of the user
+ * @return bool - return the ID of registered user for the session
+ */
+
+function registerUser($email, $firstName = null, $lastName = null, $mobilePhone = null,
+                      $imageUrl = "../web/uploads/default.jpg", $password)
+{
+
+    //Try the code for exceptions
+    try {
+
+        //Get PDO connection, using the function above
+        $pdo = getConnection();
+
+
+        //Prepare statement to execute in the Database(prevention of Database injection)
+        $statement = $pdo->prepare("INSERT INTO users (email, first_name, last_name, mobile_number, mobile_phone, image_url, password)
+                                    VALUES (?, ?, ?, ?, ?, ?, ?)");
+
+        //Execute statement
+        $statement->execute(array($email, $firstName, $lastName, $mobilePhone, $imageUrl, $password));
+
+        //Return the the ID of the registered user
+        return $pdo->lastInsertId();
 
         //Catch exception and perform action
     } catch (PDOException $e) {
