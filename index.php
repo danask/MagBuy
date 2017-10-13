57d6b3dd33646e31d480d5da798c8d081862f387
Your Name <lachezar.gadzhev@gmail.com>
2017-10-13 16:56:56 +0300

    Added index redirect code

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
 <?php
+
+// Start Session
+session_start();
+
+// Check if user have session (if user is logged)
+if (isset($_SESSION['loggedUser'])) {
+
+    //Redirect to Main
+    header('Location: view/main.php');
+
+} else {
+
+    //Redirect to Login screen
+    header('Location: view/login.php');
+}
