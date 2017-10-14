15e4d3fd9993c2d26f55f5052e17a0f7bbe49dfb
Your Name <lachezar.gadzhev@gmail.com>
2017-10-14 13:12:08 +0300

    Added unique to the database email sql script

--- controller/user/login_controller.php
+++ controller/user/login_controller.php
@@ -1,5 +1,7 @@
 <?php
 
+//TESTING
+
 function __autoload($className) {
     $className = '..\\' . $className;
     $className = str_replace("\\", "/", $className);
@@ -10,10 +12,17 @@ $user = new \model\User();
 $userDao = \model\database\UserDao::getInstance();
 
 $user->setEmail("lachezar");
+$user->setEnabled("1");
+$user->setFirstName("Lachezar");
+$user->setLastName("Gadzhev");
+$user->setMobilePhone("02130123");
+$user->setImageUrl("sdadasd");
+$user->setPassword("parola");
+$user->setRole("1");
 
 
 try {
-    $result = $userDao->checkUserExists($user);
+    $result = $userDao->registerUser($user);
     var_dump($result);
 
 } catch (PDOException $e){
