4bb6183f9231405e5d6540681b5995ccef2d5cd7
Your Name <lachezar.gadzhev@gmail.com>
2017-10-14 14:41:23 +0300

    Added Login with CSS

---
+++ view/login.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="../web/assets/css/login.css" type="text/css">
+
+    <title>Login</title>
+</head>
+<body>
+
+
+<div class="login-page">
+    <div class="form">
+        <form class="login-form">
+            <input type="text" placeholder="Your Email"/>
+            <button>login</button>
+            <p class="message">Not registered? <a href="#">Create an account</a></p>
+        </form>
+    </div>
+</div>
+
+</body>
+</html>
