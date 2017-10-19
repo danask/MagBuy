043279d008184cf510a4157821b6eed62c37636f
Your Name <lachezar.gadzhev@gmail.com>
2017-10-19 10:31:14 +0300

    Added Validation for adding review

--- controller/reviews/add_review_controller.php
+++ controller/reviews/add_review_controller.php
@@ -1,25 +1,48 @@
 <?php
 
-//Check for Session
-require_once "../../utility/no_session_main.php";
+session_start();
+
+if (isset($_SESSION['loggedUser'])) {
 
 //Autoload to require needed model files
-function __autoload($className) {
-    $className = '..\\..\\' . $className;
-    require_once str_replace("\\", "/", $className) . '.php';
+    function __autoload($className)
+    {
+        $className = '..\\..\\' . $className;
+        require_once str_replace("\\", "/", $className) . '.php';
+    }
+
+    //Validation of inputs
+
+    if (isset($_POST['rating']) && isset($_POST['title']) && isset($_POST['review']) && isset($_GET['pid'])
+        && strlen($_POST['review']) > 10 && strlen($_POST['review']) < 255
+        && strlen($_POST['title']) > 3 && strlen($_POST['title']) < 50
+        && $_POST['rating'] >= 1 && $_POST['rating'] <= 5) {
+
+        $review = new \model\Reviews();
+
+        $review->setRating(htmlentities($_POST['rating']));
+        $review->setTitle(htmlentities($_POST['title']));
+        $review->setComment(htmlentities($_POST['review']));
+        $review->setUserId($_SESSION['loggedUser']);
+        $review->setProductId($_GET['pid']);
+
+        try {
+            $reviewDao = \model\database\ReviewsDao::getInstance();
+            $reviewDao->addNewReview($review);
+
+            header("Location: ../../view/main/single.php?pid=" . $_GET['pid']);
+
+        } catch (PDOException $e) {
+
+            header("Location: ../../view/error/pdo_error.php");
+        }
+    } else {
+
+        header ("Location: ../../view/main/review.php?pid=" . $_GET['pid'] . "&error");
+    }
+
+
+} else {
+
+    header("Location: ../../view/main/index.php");
 }
-
-
-$review = new \model\Reviews();
-
-$review->setRating($_POST['rating']);
-$review->setTitle($_POST['title']);
-$review->setComment($_POST['review']);
-$review->setUserId($_SESSION['loggedUser']);
-$review->setProductId($_GET['pid']);
-
-$reviewDao = \model\database\ReviewsDao::getInstance();
-
-$reviewDao->addNewReview($review);
-
-header("Location: ../../view/main/single.php?pid=" . $_GET['pid']);
