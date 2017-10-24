3fad63a61909800e3ff02aa70dda642e6070bd26
Mihailoff <mixailov032@gmail.com>
2017-10-24 18:15:51 +0300

    started working on orders logic

---
+++ model/Order.php
@@ -0,0 +1,114 @@
+<?php
+
+
+namespace model;
+
+
+class Order
+{
+    private $id;
+    private $user_id;
+    private $created_at;
+    // STATUS CODES - 1 PROCESSING, 2 ERROR, 3 SENT, 4 FINISHED
+    private $status;
+    private $products;
+
+    /**
+     * Order constructor.
+     * @param $id
+     * @param $user_id
+     * @param $created_at
+     * @param $status
+     * @param $products
+     */
+    public function __construct($id, $user_id, $created_at, $status, $products)
+    {
+        $this->id = microtime();
+        $this->user_id = $user_id;
+        $this->created_at = date("Y-m-d H:i:s");
+        $this->status = 1;
+        $this->products = $products;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserId()
+    {
+        return $this->user_id;
+    }
+
+    /**
+     * @param mixed $user_id
+     */
+    public function setUserId($user_id)
+    {
+        $this->user_id = $user_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * @param mixed $created_at
+     */
+    public function setCreatedAt($created_at)
+    {
+        $this->created_at = $created_at;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param mixed $status
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getProducts()
+    {
+        return $this->products;
+    }
+
+    /**
+     * @param mixed $products
+     */
+    public function setProducts($products)
+    {
+        $this->products = $products;
+    }
+
+}
---
+++ model/database/OrdersDao.php
@@ -0,0 +1,53 @@
+<?php
+
+
+namespace model\database;
+
+use model\database\Connect\Connection;
+use model\Order;
+
+class OrdersDao
+{
+    //Make Singletonn
+    private static $instance;
+    private $pdo;
+
+    //Statements defined as constants
+    const ADD_NEW_ORDER = "INSERT INTO orders (id, user_id, created_at, status) VALUES (?, ?, ?, ?)";
+    const ADD_ORDER_PRODUCT = "INSERT INTO order_products (order_id, product_id) VALUES (?, ?)";
+
+    //Get connection in construct
+    private function __construct()
+    {
+
+        $this->pdo = Connection::getInstance()->getConnection();
+    }
+
+    public static function getInstance()
+    {
+
+        if (self::$instance === null) {
+            self::$instance = new OrdersDao();
+        }
+
+        return self::$instance;
+    }
+
+    function newOrder(Order $order)
+    {
+        $statement = $this->pdo->prepare(self::ADD_NEW_ORDER);
+        $statement->execute(array($order->getId(), $order->getUserId(), $order->getCreatedAt(), $order->getStatus()));
+        $orderId = $this->pdo->lastInsertId();
+
+        return $orderId;
+    }
+
+    function addOrderProduct($orderId, $productId)
+    {
+        $statement = $this->pdo->prepare(self::ADD_ORDER_PRODUCT);
+        $statement->execute(array($orderId, $productId));
+
+        return true;
+    }
+
+}
