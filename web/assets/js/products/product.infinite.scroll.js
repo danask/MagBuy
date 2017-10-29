6d02cbc3a76699dd249744bc8840eafc2c02438c
Mihailoff <mixailov032@gmail.com>
2017-10-29 17:24:38 +0200

    products on category page now load entirely with ajax

--- web/assets/js/products/product.infinite.scroll.js
+++ web/assets/js/products/product.infinite.scroll.js
@@ -1,3 +1,94 @@
+$(document).ready(function () {
+    var xhttp = new XMLHttpRequest();
+    var productsWindow = document.getElementById("productsWindow");
+    var loading = document.createElement("img");
+    var loaderDiv = document.getElementById("loader");
+    loading.setAttribute("src", "../../web/assets/images/ajax-loader.gif");
+    if (loaderDiv.children.length < 1) {
+        loaderDiv.appendChild(loading);
+    }
+    xhttp.onreadystatechange = function () {
+        if (this.status == 200 && this.readyState == 4) {
+            loaderDiv.removeChild(loading);
+            var products = JSON.parse(this.responseText);
+
+            var i = 0;
+            var content = '';
+            for (var key in products) {
+                if (products.hasOwnProperty(key)) {
+
+                    if (products[key]['percent'] != null) {
+                        // && $product['start_date'] < date("Y-m-d H:i:s")
+                        //&& $product['end_date'] > date("Y-m-d H:i:s")
+                        var promotedPrice = math.round((products[key]['price'] -
+                                    ((products[key]['price'] * products[key]['percent']) / 100)
+                                ) * 100) / 100;
+                    } else {
+                        promotedPrice = null;
+                    }
+
+                    if (key == 0) {
+                        content += '<div class="products-grid-lft">';
+                    }
+
+                    if (i == 3) {
+                        content +=
+                            '<div class="clearfix"></div></div>' +
+                            '<div class="products-grid-lft">';
+
+                        i = 0;
+                    }
+
+                    content +=
+                        '<div class="products-grd">' +
+                        '<div class="p-one simpleCart_shelfItem prd">' +
+                        '<a href="single.php?pid=' + products[key]['id'] + '">' +
+                        '<img src="' + products[key]['image_url'] + '"' +
+                        'alt="Product Image" class="img-responsive"/>' +
+                        '</a>' +
+                        '<h4>' + products[key]['title'] + '</h4>' +
+                        '<p><a class="btn btn-default btn-sm"' +
+                        'onclick="addToCart(' + products[key]['id'] +
+                        ',' + (promotedPrice != null ? promotedPrice : products[key]['price']) + ')">' +
+                        '<i class="glyphicon glyphicon-shopping-cart"></i>&nbspAdd' +
+                        '</a>&nbsp&nbsp';
+                    if (promotedPrice != null) {
+                        content +=
+                            '<span class="item_price valsa"' +
+                            'style="color: red;">$' + promotedPrice + '</span>' +
+                            '<span class="item_price promoValsa">$' + products[key]['price'] + '</span>';
+                    }
+                    else {
+                        content +=
+                            '<span class="item_price valsa">$' + products[key]['price'] + '</span>';
+                    }
+
+                    content +=
+                        '</p>' +
+                        '<div class="pro-grd">' +
+                        '<a href="single.php?pid=' + products[key]['id'] + '">View</a>' +
+                        '</div>' +
+                        '</div>' +
+                        '</div>';
+
+                    if (key == products.length - 1) {
+                        content +=
+                            '<div class="clearfix"></div></div>';
+                        $('#productsWindow').append(content);
+                    }
+
+                    i++;
+                }
+            }
+        }
+    };
+    var start = 0;
+    var subcid = location.search;
+    xhttp.open("GET", "../../controller/products/infinite_scroll_controller.php?lp="
+        + start + "&subcid=" + subcid, true);
+    xhttp.send();
+});
+
 var loadedProducts = 0;
 
 $(window).scroll(function () {
