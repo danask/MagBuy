eceffcfbd81da118bf7ba8fa5e798620b90de04b
Your Name <lachezar.gadzhev@gmail.com>
2017-10-30 12:09:14 +0200

    Related 4 products to 3 under 1200px

--- view/main/single.php
+++ view/main/single.php
@@ -280,7 +280,7 @@ require_once "../elements/navigation.php";
                                 unset($promotedPrice);
                             }
                             ?>
-                            <div class="products-grd">
+                            <div class="products-grd" id='hideUnder1200'>
                                 <div class="p-one simpleCart_shelfItem prd">
                                     <a href="single.php?pid=<?= $product['id']; ?>">
                                         <img src="<?= $product['image_url'] ?>"
