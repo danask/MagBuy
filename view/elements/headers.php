19a30c687f37d6cffa2518d0aad48b3fecfc3c3a
Your Name <lachezar.gadzhev@gmail.com>
2017-11-01 13:36:17 +0200

    Updated favicon

--- view/elements/headers.php
+++ view/elements/headers.php
@@ -10,7 +10,7 @@ if (session_status() == PHP_SESSION_NONE) {
 <head>
 
     <!-- Add Favicon -->
-    <link rel="shortcut icon" href="../../web/assets/images/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="../../web/assets/images/favicon.ico?v4" type="image/x-icon">
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
--- view/error/admin_upload_error.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: lache
- * Date: 10/17/2017
- * Time: 13:29
- */
