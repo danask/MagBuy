32cc7c8e6d7d707069061fdb8eac9ce54d9e398d
Your Name <lachezar.gadzhev@gmail.com>
2017-11-01 20:26:28 +0200

    Added styling on adding promotions

--- view/admin/products_promotions_reviews/promotion_create.php
+++ view/admin/products_promotions_reviews/promotion_create.php
@@ -20,9 +20,9 @@ require_once "../../../utility/blocked_user_dir_back.php";
 <div class="page">
     <form action="../../../controller/admin/products_promotions_reviews/new_product_promotion_controller.php"
           method="post">
-        Percent<input type="number" name="percent" placeholder="%" required><br>
-        Start<input type="datetime-local" name="start_date" placeholder="Start Date" required><br>
-        End<input type="datetime-local" name="end_date" placeholder="End Date" required><br>
+        Percent <input type="number" name="percent" placeholder="%" required><br>
+        Start <input type="datetime-local" name="start_date" placeholder="Start Date" required><br>
+        End <input type="datetime-local" name="end_date" placeholder="End Date" required><br>
         <input type="hidden" name="product_id" value="<?= $_GET['pid'] ?>">
         <input type="submit" name="submit" value="Add promotion!">
     </form>
